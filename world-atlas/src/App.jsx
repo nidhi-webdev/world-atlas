fd2f8b45b6af9d355ad7d278b321c90296662c65
nidhi <kumarinidhi0594@gmail.com>
2026-03-11 23:19:33 +0100

    Router

--- world-atlas/src/App.jsx
+++ world-atlas/src/App.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
+import { createBrowserRouter } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Contact } from "./pages/Contact"
+import { Country } from "./pages/Country"
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "about", element: <About /> },
+  { path: "contact", element: <Contact /> },
+  { path: "country", element: <Country /> }
+])
+
+
 
 const App = () => {
   return (
