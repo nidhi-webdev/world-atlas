2cb0adefdd5a12cd83f6851217c106cf053706ca
nidhi <kumarinidhi0594@gmail.com>
2026-03-12 14:23:36 +0100

    Header

--- world-atlas/src/components/ui/Hearder.jsx
+++ world-atlas/src/components/ui/Hearder.jsx
@@ -1,3 +1,22 @@
+import { NavLink } from "react-router-dom"
+
 export const Headers = () => {
-    return <h1> Headers Page </h1>
+    return <header>
+        <div className="bg-gray-800 pt-8 pb-8 flex justify-between items-center">
+            <div className="ml-[15%]">
+                <NavLink to="/">
+                    <h1 className=" font-extrabold text-3xl"> WorldAtlas </h1>
+                </NavLink>
+            </div>
+
+            <nav className="mr-[15%]">
+                <ul className="flex gap-6">
+                    <li className="hover:text-amber-700"> <NavLink to="/"> Home </NavLink> </li>
+                    <li className="hover:text-amber-700"> <NavLink to="/about"> About </NavLink> </li>
+                    <li className="hover:text-amber-700"> <NavLink to="/country"> Country </NavLink> </li>
+                    <li className="hover:text-amber-700"> <NavLink to="/contact"> Contact </NavLink> </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 }
