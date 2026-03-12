af79f02e964e80576f626edd9ce13c4320ed73d7
nidhi <kumarinidhi0594@gmail.com>
2026-03-12 14:05:15 +0100

    Error Page complete

--- world-atlas/src/pages/ErrorPage.jsx
+++ world-atlas/src/pages/ErrorPage.jsx
@@ -1,11 +1,12 @@
-import { useRouteError } from "react-router-dom"
+import { NavLink, useRouteError } from "react-router-dom"
 
 export const ErrorPage = () => {
     const error = useRouteError()
     console.log("Error in the page", error);
 
-    return <div className="flex justify-center items-center min-h-screen">
+    return <div className="flex flex-col justify-center items-center min-h-screen gap-5">
         <h1> Opps! An error occurred </h1>
         {error && <p> {error.data}</p>}
+        <NavLink to="/" className="px-4 py2 bg-gray-600 flex flex-col justify-center rounded"> Go Home </NavLink>
     </div>
 }
