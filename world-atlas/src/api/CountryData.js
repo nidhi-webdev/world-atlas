e1624e35394bc0600418bf74a22bbe2b900a820b
nidhi <kumarinidhi0594@gmail.com>
2026-03-12 15:11:29 +0100

    getting the json data in the file

--- world-atlas/src/api/CountryData.js
+++ world-atlas/src/api/CountryData.js
@@ -1,4 +1,4 @@
-[
+export const countryData = [
   {
     "id": 1,
     "countryname": "Germany",
--- world-atlas/src/pages/About.jsx
+++ world-atlas/src/pages/About.jsx
@@ -1,11 +1,33 @@
+import { countryData } from "../api/CountryData.js"
+
 export const About = () => {
-    return <section>
-        <h2> Here are the Interesting Facts <br /> we're proud of </h2>
-        <div> 
+    return <div>
+        <div>
+            <h2> Here are the Interesting Facts <br /> we're proud of </h2>
+
+        </div>
+        <div>
             <p>India </p>
             <p><span> Capital: Delhi </span></p>
             <p><span> Population: Delhi </span></p>
-             <p><span> Interesting Facts: Delhi </span></p>
+            <p><span> Interesting Facts: Delhi </span></p>
         </div>
-    </section>
+
+        {countryData.map((country) => {
+            const{id, countryname, capital, population, interestingfacts} = country
+            return <>
+                <div key={id}>
+                    <h2> Here are the Interesting Facts <br /> we're proud of </h2>
+
+                </div>
+                <div>
+                    <p>{countryname} </p>
+                    <p><span> Capital: {capital} </span></p>
+                    <p><span> Population: {population} </span></p>
+                    <p><span> Interesting Facts: {interestingfacts} </span></p>
+                </div>
+            </>
+        })
+        }
+    </div>
 }
