201550177689279b55f12edbba5c43bed2167203
nidhi <kumarinidhi0594@gmail.com>
2026-03-13 22:31:04 +0100

    tailwind

--- world-atlas/src/pages/Contact.jsx
+++ world-atlas/src/pages/Contact.jsx
@@ -1,26 +1,36 @@
 export const Contact = () => {
-    return <div>
-        <h2> Contact Us </h2>
-        <form>
-            <input 
-            type="text" 
-            required 
-            autoComplete="false" 
-            placeholder="Enter Your Name" 
-            name="username"  />
+    return <div className="flex flex-col justify-center items-center mt-20">
+        <h2 className="font-bold text-3xl mb-8 "> Contact Us </h2>
+        <div className="flex flex-col justify-center items-center">
+            <form action="">
+                <input
+                    type="text"
+                    required
+                    autoComplete="false"
+                    placeholder="Enter Your Name"
+                    name="username"
+                    className="border border-gray-600 flex flex-col mb-8 " />
 
-            <input 
-            type="email" 
-            required 
-            autoComplete="false" 
-            placeholder="Enter Your Email" 
-            name="email"  />
+                <input
+                    type="email"
+                    required
+                    autoComplete="false"
+                    placeholder="Enter Your Email"
+                    name="email"
+                    className="border border-gray-600 flex flex-col mb-8" />
 
-            <textarea 
-            rows="10" 
-            placeholder="Entre Your Message" 
-            required 
-            autoComplete="false"></textarea>
-        </form>
+                <textarea
+                    rows="10"
+                    placeholder="Entre Your Message"
+                    required
+                    autoComplete="false"
+                    className="border border-gray-600 flex flex-col"></textarea>
+
+                <button type="submit"> Send </button>
+            </form>
+        </div>
+
     </div>
+
+
 }
