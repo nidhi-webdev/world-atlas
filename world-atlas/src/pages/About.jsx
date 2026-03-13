bebc018ff09a3831cb529a638a90e06e81b284dd
nidhi <kumarinidhi0594@gmail.com>
2026-03-13 21:45:23 +0100

    About page design

--- world-atlas/src/pages/About.jsx
+++ world-atlas/src/pages/About.jsx
@@ -1,33 +1,25 @@
 import { countryData } from "../api/CountryData.js"
 
 export const About = () => {
-    return <div>
-        <div>
-            <h2> Here are the Interesting Facts <br /> we're proud of </h2>
+    return <div className=" mt-10 px-[10%]">
+        <h2 className="font-extrabold text-2xl text-center"> Here are the Interesting Facts  </h2>
+        <p className="font-extrabold text-2xl text-center mb-8"> we're proud of </p>
+        <div className="grid grid-cols-3 gap-6 ">
+            {countryData.map((country) => {
+                const { id, countryname, capital, population, interestingfacts } = country
+                return <>
+                    <div className="flex bg-gray-700 rounded-xl p-6 flex-col gap-3 shadow-lg hover:scale-105 transition-transform duration-300 cursor-pointer " key={id}>
+                        <h3 className="font-extrabold text-xl text-white border-b border-gray-500 pb-2">{countryname} </h3>
+                        <p className="text-white font-semibold"><span> Capital: {capital} </span></p>
+                        <p className="text-white font-semibold"><span> Population: {population} </span></p>
+                        <p className="text-white font-semibold"><span> Interesting Facts: {interestingfacts} </span></p>
+                    </div>
 
+                </>
+            })
+            }
         </div>
-        <div>
-            <p>India </p>
-            <p><span> Capital: Delhi </span></p>
-            <p><span> Population: Delhi </span></p>
-            <p><span> Interesting Facts: Delhi </span></p>
-        </div>
 
-        {countryData.map((country) => {
-            const{id, countryname, capital, population, interestingfacts} = country
-            return <>
-                <div key={id}>
-                    <h2> Here are the Interesting Facts <br /> we're proud of </h2>
 
-                </div>
-                <div>
-                    <p>{countryname} </p>
-                    <p><span> Capital: {capital} </span></p>
-                    <p><span> Population: {population} </span></p>
-                    <p><span> Interesting Facts: {interestingfacts} </span></p>
-                </div>
-            </>
-        })
-        }
     </div>
 }
