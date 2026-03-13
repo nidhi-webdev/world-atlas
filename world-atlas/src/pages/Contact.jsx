7e6ea2110be3da88615699638ab64ebe7e327ec2
nidhi <kumarinidhi0594@gmail.com>
2026-03-13 22:21:28 +0100

    Contact page

--- world-atlas/src/pages/Contact.jsx
+++ world-atlas/src/pages/Contact.jsx
@@ -1,3 +1,26 @@
 export const Contact = () => {
-    return <h1> Contact Page </h1>
+    return <div>
+        <h2> Contact Us </h2>
+        <form>
+            <input 
+            type="text" 
+            required 
+            autoComplete="false" 
+            placeholder="Enter Your Name" 
+            name="username"  />
+
+            <input 
+            type="email" 
+            required 
+            autoComplete="false" 
+            placeholder="Enter Your Email" 
+            name="email"  />
+
+            <textarea 
+            rows="10" 
+            placeholder="Entre Your Message" 
+            required 
+            autoComplete="false"></textarea>
+        </form>
+    </div>
 }
