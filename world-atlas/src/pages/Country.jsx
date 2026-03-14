b891d83e71b20931de19922ce74f2541b4bca051
nidhi <kumarinidhi0594@gmail.com>
2026-03-14 17:21:26 +0100

    New Hook, useTransition

--- world-atlas/src/pages/Country.jsx
+++ world-atlas/src/pages/Country.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useTransition } from "react"
 
 
 
 export const Country = () => {
+const [isPending, startTransition] = useTransition()
+
+useEffect({
+
+}, [])
+
+ if(isPending) return <h1> Loading... </h1>
     return <div></div>
 }
