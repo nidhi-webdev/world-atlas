7cbe785dbb19092563ab7deba684d3b1baffcc6f
nidhi <kumarinidhi0594@gmail.com>
2026-03-14 16:53:57 +0100

    MADE the icon dynamic

--- world-atlas/src/components/ui/Footer.jsx
+++ world-atlas/src/components/ui/Footer.jsx
@@ -1,11 +1,15 @@
 import { footerData } from "../../api/FooterApi"
 import { MdPlace } from "react-icons/md";
+import { IoCallSharp } from "react-icons/io5";
+import { TbMailPlus } from "react-icons/tb";
 
 
 export const Footer = () => {
 
     const footerIcon = {
-        MdPlace : <MdPlace />
+        MdPlace : <MdPlace />,
+        IoCallSharp: <IoCallSharp />,
+        TbMailPlus: <TbMailPlus />
     }
 
     console.log("footerData", footerData);
@@ -14,7 +18,7 @@ export const Footer = () => {
         {footerData.map((curData, index) => {
             const { icon, title, details } = curData
             return <div key={index}>
-                <p> {footerIcon.MdPlace} </p>
+                <p> {footerIcon[icon]} </p>
                 <p> {title} </p>
                 <p> {details} </p>
             </div>
