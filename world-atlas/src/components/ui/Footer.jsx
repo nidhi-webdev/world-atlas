dc69087830fa5e14be7882635aa58b8101ec9531
nidhi <kumarinidhi0594@gmail.com>
2026-03-14 16:50:16 +0100

    footer and icon

--- world-atlas/src/components/ui/Footer.jsx
+++ world-atlas/src/components/ui/Footer.jsx
@@ -1,3 +1,23 @@
+import { footerData } from "../../api/FooterApi"
+import { MdPlace } from "react-icons/md";
+
+
 export const Footer = () => {
-    return <h1> Footer Page </h1>
+
+    const footerIcon = {
+        MdPlace : <MdPlace />
+    }
+
+    console.log("footerData", footerData);
+
+    return <footer>
+        {footerData.map((curData, index) => {
+            const { icon, title, details } = curData
+            return <div key={index}>
+                <p> {footerIcon.MdPlace} </p>
+                <p> {title} </p>
+                <p> {details} </p>
+            </div>
+        })}
+    </footer>
 }
