ed90b6dacf67f0d15f3721643a0c83f4f84c117f
nidhi <kumarinidhi0594@gmail.com>
2026-03-15 21:26:09 +0100

    getting the data

--- world-atlas/src/pages/Country.jsx
+++ world-atlas/src/pages/Country.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useTransition } from "react"
+import { useEffect, useState, useTransition } from "react"
 import { getCountryData } from "../api/api"
 import { Loader } from "../components/ui/Loader"
 
@@ -6,11 +6,12 @@ import { Loader } from "../components/ui/Loader"
 
 export const Country = () => {
     const [isPending, startTransition] = useTransition()
+    const [countries, setCountries] = useState([])
 
     useEffect(() => {
         startTransition(async () => {
             const res = await getCountryData()
-            console.log("Resonse", res.data);
+            setCountries(res.data)
 
         })
     }, [])
@@ -18,6 +19,16 @@ export const Country = () => {
     if (isPending) return <Loader />
 
     return <div className="grid grid-cols-4">
+        {countries.map((curdata, index) => {
+            console.log("countries", countries);
 
+            const { capital, flags, name, population, region, } = curdata
+
+            return <>
+                <div key={index}> <h1 className="text-white"> {capital}  </h1>
+
+                </div>
+            </>
+        })}
     </div>
 }
