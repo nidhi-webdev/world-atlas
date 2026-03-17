edaa73d9e29c0c216dc77a4c2420e673a2207727
nidhi <kumarinidhi0594@gmail.com>
2026-03-17 13:44:39 +0100

    Making the usestate for search and also passing the data

--- world-atlas/src/pages/Country.jsx
+++ world-atlas/src/pages/Country.jsx
@@ -8,6 +8,8 @@ import { CountryCard } from "./CountryCard"
 export const Country = () => {
     const [isPending, startTransition] = useTransition()
     const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState()
+    const [filter, setFilter] = useState()
 
     useEffect(() => {
         startTransition(async () => {
@@ -20,9 +22,15 @@ export const Country = () => {
     if (isPending) return <Loader />
 
     return <div className=" mt-10 px-[10%]">
+        <SearchFilter
+            search={search}
+            setSearch={setSearch}
+            filter={filter}
+            setFilter={setFilter} />
+
         <div className="grid grid-cols-4 gap-6">
             {countries.map((countryData, index) => {
-                return <CountryCard countryData={countryData} key={index}  />
+                return <CountryCard countryData={countryData} key={index} />
             })}
         </div>
     </div>
