7dde494aa888c8dd1e036c21356180dd924e699b
nidhi <kumarinidhi0594@gmail.com>
2026-03-18 19:20:18 +0100

    Search Value

--- world-atlas/src/components/ui/SearchFilter.jsx
+++ world-atlas/src/components/ui/SearchFilter.jsx
@@ -1,3 +1,16 @@
-export const SearchFilter = () => {
-    return <div> SearchFilter </div>
+export const SearchFilter = ({ search, setSearch, filter, setFilter }) => {
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value)
+        console.log("Types Value", e.target.value);
+        
+    }
+
+    return <div>
+        <input
+            type="text"
+            value={search}
+            placeholder="Search"
+            onChange={handleSearch} />
+    </div>
 }
