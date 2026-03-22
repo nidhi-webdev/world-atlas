bc522c58f6261e5d780ee88c3340299e2db2c3be
nidhi <kumarinidhi0594@gmail.com>
2026-03-22 12:54:54 +0100

    breadcrumbs on small device

--- world-atlas/src/components/ui/Hearder.jsx
+++ world-atlas/src/components/ui/Hearder.jsx
@@ -19,8 +19,8 @@ export const Headers = () => {
                 </NavLink>
             </div>
 
-            <nav className={`mr-[15%] md:block ${show ? "absolute top-24 left-0 w-full bg-gray-800 flex py-4 z-50" : "hidden"} `}>
-                <ul className="flex flex-col md:flex-row items-center gap-6">
+            <nav className={`mr-[15%] max-[770px]:hidden ${show ? "max-[770px]:block! max-[770px]:absolute max-[770px]:top-24 max-[770px]:left-0 max-[770px]:w-full max-[770px]:bg-gray-800 max-[770px]:py-4 max-[770px]:z-50" : ""} `}>
+                <ul className="flex max-[770px]:flex-col items-center gap-6">
                     <li className="hover:text-amber-700"> <NavLink to="/"> Home </NavLink> </li>
                     <li className="hover:text-amber-700"> <NavLink to="/about"> About </NavLink> </li>
                     <li className="hover:text-amber-700"> <NavLink to="/country"> Country </NavLink> </li>
@@ -28,7 +28,7 @@ export const Headers = () => {
                 </ul>
             </nav>
 
-            <div className="md:hidden mr-[15%] text-2xl">
+            <div className="hidden max-[770px]:block mr-[15%] text-2xl">
                 <button onClick={handleButtonToggle}>
                     <GiHamburgerMenu />
                 </button>
